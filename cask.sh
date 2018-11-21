066d76217a76c5f2a3f6bb6423e96c204ddc323c
Maxim Sukharev <maxim@sourced.tech>
2018-11-21 23:58:35 +0100

    add cask.sh

---
+++ cask.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+brew cask install iterm2
+brew cask install java8
+brew cask install 1password
+brew cask install firefox
+brew cask install google-chrome
+brew cask install slack
+brew cask install vlc
+brew cask install tunnelbear
+brew cask install dropbox
+brew cask install docker
+brew cask install visual-studio-code
+brew cask install skype
+brew cask install anki
+brew cask install transmission
+brew cask install zoomus
+brew cask install keepingyouawake
