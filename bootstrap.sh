1ae4fba6889e8218eda363211ccae7d0359db4ce
Maxim Sukharev <maxim@sourced.tech>
2018-11-22 00:47:41 +0100

    create bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Bootstraps new mac
+
+source ./macos.sh
+source ./brew.sh
+source ./cask.sh
+
+# Create links
+
+ln -s $(pwd)/.bashrc $HOME/.bashrc
+ln -s $(pwd)/.bash_profile $HOME/.bash_profile
+ln -s $(pwd)/.bash_prompt $HOME/.bash_prompt
+ln -s $(pwd)/.aliases $HOME/.aliases
+ln -s $(pwd)/.exports $HOME/.exports
+
+ln -s $(pwd)/.gitconfig $HOME/.gitconfig
+ln -s $(pwd)/.gitignore $HOME/.gitignore
+
+mkdir -p $HOME/.vim
+mkdir -p $HOME/.vim/swap
+mkdir -p $HOME/.vim/backup
+ln -s $(pwd)/.vimrc $HOME/.vimrc
+ln -s $(pwd)/.vim/CSApprox.txt $HOME/.vim/CSApprox.txt
+ln -s $(pwd)/.vim/after $HOME/.vim/after
+ln -s $(pwd)/.vim/autoload $HOME/.vim/autoload
+ln -s $(pwd)/.vim/colors $HOME/.vim/colors
+ln -s $(pwd)/.vim/plugin $HOME/.vim/plugin
+ln -s $(pwd)/.vim/tags $HOME/.vim/tags
