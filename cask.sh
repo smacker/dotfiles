3fd3e04e9a3aba5babda52d6818e59802df8cb3e
Maxim Sukharev <maxim.sukharev@foodpanda.com>
2021-09-22 09:59:55 +0800

    group cask programs

--- cask.sh
+++ cask.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
+# must have
 brew cask install iterm2
 brew cask install bitwarden
 brew cask install firefox
 brew cask install google-chrome
-brew cask install slack
-brew cask install vlc
-brew cask install tunnelbear
 brew cask install dropbox
+
+# utils
+brew cask install tunnelbear
+brew cask install keepingyouawake
+
+# entertainment
+brew cask install transmission
+brew cask install spotify
+brew cask install vlc
+
+# dev
 brew cask install docker
 brew cask install visual-studio-code
-brew cask install skype
-brew cask install transmission
+
+# work
+brew cask install slack
 brew cask install zoomus
-brew cask install keepingyouawake
-brew cask install spotify
+brew cask install skype
