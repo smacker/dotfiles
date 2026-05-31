d5f16956177e4db9487d861d930f5b47f42e0abd
Maxim Sukharev <max@flp.studio>
2026-05-31 18:57:18 +0800

    update cask list

--- cask.sh
+++ cask.sh
@@ -1,43 +1,33 @@
 #!/usr/bin/env bash
 
 # must have
-brew install --cask iterm2
+brew install --cask ghostty
 brew install --cask bitwarden
-brew install --cask firefox
-brew install --cask google-chrome
 brew install --cask brave-browser
+brew install --cask google-chrome
 brew install --cask dropbox
 brew install --cask google-drive
+brew install --cask tailscale-app
 
 # utils
-brew install --cask tunnelbear
 brew install --cask keepingyouawake
 brew install --cask tomatobar
+brew install --cask rar
 
 # entertainment
 brew install --cask transmission
 brew install --cask spotify
-brew install --cask vlc
-brew install --cask plex-media-server
 
 # communications
 brew install --cask telegram
 brew install --cask whatsapp
 
 # dev
-brew install --cask docker
-brew install docker-compose # compose is a plugin now and needs to be installed separately
-brew install --cask visual-studio-code
-brew install --cask android-platform-tools
-brew install --cask postman
-brew install --cask dbeaver-community
-brew install --cask session-manager-plugin
+brew install --cask zed
+brew install --cask beekeeper-studio
 
 # work
 brew install --cask slack
-brew install --cask zoom
-brew install --cask skype
-brew install --cask loom
 
 # other
 brew install --cask anki
@@ -47,6 +37,5 @@ brew install --cask raycast
 brew install --cask todoist
 
 # fonts
-brew tap homebrew/cask-fonts
 brew install --cask font-fira-code-nerd-font
 brew install --cask font-monaspace
